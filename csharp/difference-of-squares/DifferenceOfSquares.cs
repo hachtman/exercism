9f40c22d6cfe836ad852322bd4c6996a2a593de6
Hachtman <jack.fuller@valsys.io>
2017-07-26 19:35:17 +0100

    csharp difference of squares

---
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class Squares
+{
+    public static int SquareOfSums(int max)
+    {
+        int count = 0;
+        for (int i = 0; i <= max; i++) {
+            count += i;
+        }
+        return count * count;
+    }
+
+    public static int SumOfSquares(int max)
+    {
+        int count = 0;
+        for (int i = 0; i <= max; i++) {
+            count += i * i;
+        }
+        return count;
+    }
+
+    public static int DifferenceOfSquares(int max)
+    {
+        return (SquareOfSums(max) - SumOfSquares(max));
+    }
+}
