1dce4b529d3f378c4e04f540d0f14646fb29c072
Hachtman <jack.fuller@valsys.io>
2017-07-26 18:18:49 +0100

    csharp leap year checker

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Linq;
 
 public static class Bob
 {
+
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+
+        if (statement.Length == 0 || String.IsNullOrWhiteSpace(statement))
+        {
+            return "Fine. Be that way!";
+        }
+        else if (CheckNoAlphas(statement) && statement[statement.Length - 1] == '?')
+        {
+            return "Sure.";
+        }
+        else if (Char.IsNumber(statement[0]) && Char.IsNumber(statement[statement.Length -1]))
+        {
+            return "Whatever.";
+        }
+        else if (!CheckNoAlphas(statement) && statement.ToUpper() == statement)
+        {
+            return "Whoa, chill out!";
+        }
+        else if (statement.TrimEnd()[statement.TrimEnd().Length - 1] == '?') {
+            return "Sure.";
+        }
+        else
+        {
+            return "Whatever.";
+        }
     }
+
+    public static bool CheckNoAlphas(string statement)
+    {
+        bool result = !statement.Any(char.IsLetter);
+        return result;
+    }
+
 }
